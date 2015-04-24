039f363b8944a471cfb6910173f1679735c06f8e
zhaozheng <627364744@qq.com>
2015-04-24 15:44:08 +0800

    命令
    1-2

---
+++ StudyCommandDemo1_1/StudyCommandDemo1_1/Commands/CustomCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StudyCommandDemo1_1.Commands
+{
+    public class CustomCommand
+    {
+        private static RoutedUICommand query;
+        public static RoutedUICommand Query { get { return query; } }
+
+        static CustomCommand() 
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.R, ModifierKeys.Control, "Ctrl+R"));
+            query = new RoutedUICommand("Query", "Query", typeof(CustomCommand), inputs);
+        }
+    }
+}
--- StudyCommandDemo1_1/StudyCommandDemo1_1/MainWindow.xaml.cs
+++ StudyCommandDemo1_1/StudyCommandDemo1_1/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace StudyCommandDemo1_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool IsSave = false;
         public MainWindow()
         {
             InitializeComponent();
+            CommandBinding binding = new CommandBinding(ApplicationCommands.Cut);
+            CommandBinding SaveBinding = new CommandBinding(ApplicationCommands.Save);
+            binding.Executed += NewCommand_Executed;
+            SaveBinding.Executed+=SaveCommand_Executed;
+            SaveBinding.CanExecute += SaveCommand_CanExecuted;
+            this.CommandBindings.Add(binding);
+            this.CommandBindings.Add(SaveBinding);
+        }
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e) 
+        {
+            MessageBox.Show("New");
+        }
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Save");
+            IsSave = false;
+        }
+
+        private void tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            IsSave = true;
+        }
+        private void SaveCommand_CanExecuted(object sender, CanExecuteRoutedEventArgs e) 
+        {
+            e.CanExecute = IsSave;
         }
     }
 }
